ccc5638d816f4834ff9ae84f61c196122c90e628
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-11-23 16:02:38 +0200

    Complete the task

--- src/main/java/org/example/BusinessCar.java
+++ src/main/java/org/example/BusinessCar.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class BusinessCar extends PassengerBaseCar {
 
-    private static final int COMFORT_TIER = 5;
+    private static final int COMFORT_TIER = 10;
 
     public BusinessCar(int passengerCount, int bagsCount) {
         super(passengerCount, bagsCount, COMFORT_TIER);
--- src/main/java/org/example/CoachCar.java
+++ src/main/java/org/example/CoachCar.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class CoachCar extends PassengerBaseCar {
 
-    private static final int COMFORT_TIER = 10;
+    private static final int COMFORT_TIER = 5;
 
     public CoachCar(int passengerCount, int bagsCount) {
         super(passengerCount, bagsCount, COMFORT_TIER);
